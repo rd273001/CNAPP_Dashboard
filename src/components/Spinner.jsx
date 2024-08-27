23cd0a1399d93868cc9a29637e0b98b9fcc592e1
rd273001 <rd273001@gmail.com>
2024-08-27 21:15:33 +0530

    Updated Spinner

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Spinner = () => {
   return (
     <div className='flex flex-1 items-center justify-center'>
-      <hr className='border-[5px] size-12 animate-spin border-t-blue-500 border-b-black rounded-full' />
+      <hr className='border-[5px] size-10 animate-[spin_600ms_linear_infinite] border-gray-200 border-t-blue-700 rounded-full' />
     </div>
   );
 };
